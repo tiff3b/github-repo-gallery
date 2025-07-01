f24831497eab23aca84d20d59e9530d62b7c49a2
tiff3b <tiffany3berger@gmail.com>
2025-07-01 10:06:37 -0400

    Fetch and display repo data

--- github-repo-gallery-main/js/script.js
+++ github-repo-gallery-main/js/script.js
@@ -1,7 +1,9 @@
 //Profile info will appear here
 const overview = document.querySelector(".overview");
 const userName = "tiff3b";
+const repoList = document.querySelector(".repo-list");
 
+//get user info
 const getData = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${userName}`);
     const data = await userInfo.json();
@@ -24,4 +26,29 @@ const displayUserInfo = function (data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div> `;
     overview.append(div);
+    //call function that fetches repos
+    getRepos();
+};
+
+//get public repos and sort by recent updatd and show up to 100 repos per page
+const getRepos = async function (){
+    const publicRepos = await fetch (`https://api.github.com/users/${userName}/repos?sort=updated&per_page=100`);
+    const repoData = await publicRepos.json();
+    //verify repos are caught
+    console.log(repoData);
+    //call function for repo info created below and pass the json above
+    displayPublicRepos(repoData);
+};
+
+//choose data from repos to display
+//use repos as the parameter so functions accepts data returned from API call above
+const displayPublicRepos = function (repos){
+    //loop and create list item for each repo and give a class and element for the repo name
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        //append to global variable the selects the ul 
+        repoList.append(repoItem);
+    }
 };
